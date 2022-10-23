ced0b0dd762f43b7904689a253544f9fb77fd399
cdmxp <58542703+cdmxp@users.noreply.github.com>
2022-10-24 07:58:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-cupsd package/lean/cupsd
+git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
