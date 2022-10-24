0e0dc6ff8bcbc764cba8e8f51195a8d01466bc4c
cdmxp <58542703+cdmxp@users.noreply.github.com>
2022-10-24 19:31:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改openwrt登陆地址,把下面的192.168.6.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+# 修改内核6.0
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.0/g' ./target/linux/x86/Makefile
+# 修改概览里时间显示为中文数字
+# sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/arm/index.htm
+sed -i 's/os.date()/os.date("%Y年%m月%d日") .. " " .. translate(os.date("%A")) .. " " .. os.date("%X")/g' package/lean/autocore/files/x86/index.htm
